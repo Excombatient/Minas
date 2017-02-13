4deb255415b732b3f12167d6e798b33cb104d918
Marc <Marc@DESKTOP-NI7RM2E>
2017-02-13 17:29:09 +0100

    Version 1.3 Marc 17:28

--- Minas/src/minas/Minas.java
+++ Minas/src/minas/Minas.java
@@ -1,4 +1,4 @@
-/*
+ /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -10,6 +10,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -20,18 +21,24 @@ import javafx.stage.Stage;
 public class Minas extends Application {
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Start");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+        Pane root = new Pane();
+        for(int i = 3;i<20;i++){
+            for(int n = 0; n<20; n++){
+                Rectangle btn = new Rectangle();
+                btn.setText("   ");
+                btn.setPos((i-3)+"-"+n);  
+                btn.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                    System.out.println("Position: "+ btn.getPos());
+                   }
+                });
+                btn.setLayoutX(25*n);
+                btn.setLayoutY(25*i); 
+                root.getChildren().add(btn);
             }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+            
+        }
         
         Scene scene = new Scene(root, 500, 500);
         
---
+++ Minas/src/minas/Rectangle.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minas;
+
+import javafx.scene.control.Button;
+
+/**
+ *
+ * @author Marc
+ */
+public class Rectangle extends Button{
+    private String pos;
+
+    public String getPos() {
+        return pos;
+    }
+
+    public void setPos(String pos) {
+        this.pos = pos;
+    }
+    
+    
+    
+    
+}
